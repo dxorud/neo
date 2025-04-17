0fd459811712b532a10a5e31ad941132d6c3b4fc
dxorud <lmkltk55@gmail.com>
2025-04-17 10:43:42 +0900

    myterror

--- python/pythonData/p471_orcleConn.py
+++ python/pythonData/p471_orcleConn.py
@@ -1,20 +1,21 @@
 import cx_Oracle
+import pandas as pd
+import matplotlib.pyplot as plt
+from pandas import Series
 
 cx_Oracle.init_oracle_client(lib_dir="/usr/local/OracleXE/instantclient_19_26")
+
+plt.rcParams['font.family'] = 'NanumBarunGothic'
 
 conn = None
 cur = None
 
 try:
-    loginfo = 'hr/1234@192.168.1.131:1521/xe'
+    loginfo = 'hr/1234@192.168.1.111:1521/xe'
     conn = cx_Oracle.connect(loginfo)
-
-    print(type(conn))
-
     cur = conn.cursor()
-    print(type(cur))
 
-    sql = 'select power(2,10) from dual'
+    sql = 'select * from country_summary_top_10'
     cur.execute(sql)
 
     for item in cur:
@@ -26,8 +27,8 @@ except Exception as err:
 finally:
     if cur != None:
         cur.close()
-    
+
     if conn != None:
         conn.close()
-        
+
 print("finished...")
